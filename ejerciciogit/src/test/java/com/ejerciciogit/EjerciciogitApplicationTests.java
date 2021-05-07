772f426d4a9627b45b318b79b3ff187f9553b887
angelica <angelicamartinezsastoque@gmail.com>
2021-05-07 17:18:57 -0500

    creacion proyecto

---
+++ ejerciciogit/src/test/java/com/ejerciciogit/EjerciciogitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejerciciogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjerciciogitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
